98d591cbf8d7d4be4ee9108e044666fbfc23b0e5
dcatcherex <dcatcherex@yahoo.com>
2023-07-06 07:12:00 +0700

    update

---
+++ src/components/Popout.jsx
@@ -0,0 +1,62 @@
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment } from 'react'
+
+
+
+const PopOut = ({text,content_img=""}) => {
+  return (
+    <div>
+      <div className="">
+      <Popover className="">
+        {({ open }) => (
+          <>
+            <Popover.Button
+              className={`
+                ${open ? '' : 'text-opacity-90'}
+                btn`}
+            >
+              <span>{text}</span>
+              
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                  <img src={content_img} alt="" />
+                  <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                    
+                  </div>
+                  <div className="bg-gray-50 p-4">
+                    <a
+                      href="##"
+                      className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                    >
+                      <span className="flex items-center">
+                        <span className="text-sm font-medium text-gray-900">
+                          Documentation
+                        </span>
+                      </span>
+                      <span className="block text-sm text-gray-500">
+                        Start integrating products and tools
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </div>
+    </div>
+  );
+};
+
+export default PopOut;
--- src/pages/Cye.jsx
+++ src/pages/Cye.jsx
@@ -1,36 +1,75 @@
 import timetable1 from "../assets/images/camsis-timetable-1.png";
 import timetable2 from "../assets/images/camsis-timetable-2.png";
-import cye1 from "../assets/images/cye/cye.jpg"
-import cye2 from "../assets/images/cye/cye_2.jpg"
-import cye3 from "../assets/images/cye/cye_3.jpg"
-import cye4 from "../assets/images/cye/cye_4.jpg"
-import cye5 from "../assets/images/cye/cye_5.jpg"
-import cye6 from "../assets/images/cye/cye_6.jpg"
-import cyetop from "../assets/images/cye/cye_top.jpg"
+import cye1 from "../assets/images/cye/cye.jpg";
+import cye2 from "../assets/images/cye/cye_2.jpg";
+import cye3 from "../assets/images/cye/cye_3.jpg";
+import cye4 from "../assets/images/cye/cye_4.jpg";
+import cye5 from "../assets/images/cye/cye_5.jpg";
+import cye6 from "../assets/images/cye/cye_6.jpg";
+import cyetop from "../assets/images/cye/cye_top.jpg";
+import cyecourseoutline from "../assets/images/cye/courseoutline.jpg"
 
 import { CheckCircleIcon } from "@heroicons/react/20/solid";
+
+import PopOut from "../components/Popout";
+
+const images = [cye1,cye2,cye3,cye4,cye5,cye6]
+
+  
+
+const hero = (
+  "Situated in the historic town of Cambridge, the course combines both academic knowledge for university and key skills for future careers. It is perfect for gaining an introduction into the study of entrepreneurship at the university level and develop skills essential for success such as leadership skills and communication skills. Here, you will get to study with prominent academics from the world-renowned University of Cambridge, live in its centuries-old college accommodations just like their students, dine at its historic dining halls, meet bright young minds from Cambridge who would be your supervisors and connect with classmates. This truly is a once-in-a-lifetime experience that you wouldn’t want to miss."
+)
+
+const topics = ['Entrepreneurship fundamentals',"Innovation and “Design Thinking”","Creating differentiated products and services","Understanding customers","Growth strategies","Business model innovation","Finance","Business plan preparation"
+]
+
+const teachingApproaches = ["Lectures","Oxbridge-style supervisions","In-class experiential activities", "Project-based learning","Small group workshops(with local business & start-up companies"]
+
+const includeItems = ["Course Fees","All teaching, small group seminars, and skills development workshops","Accommodation for 14 nights in college room","Venue and study materials","3 meals a day","Formal Dinner (Cambridge’s traditional college banquet)","All programmed visits and excursions","All transportation including airport transfers","A graduation ceremony","A pre-departure orientation","Travel insurance","A supervisor accompanying the group throughout the programme from airport departure to home arrival"
+]
+
+const notIncludeItems = ["International fligh ticket","Visa application fees (if applicable)"]
+
+const highlights = ["Taught by Cambridge professors/lecturers","Certificate of attendance","University-level subject in a campus environment","Mini Hackathon experience (the winning team gets a prize)","Workshops with local businesses and start-up companies","Skills development workshops for future success","Real-life experience as a Cambridge student","Networking opportunities with Cambridge faculties & students/ successful entrepreneurs & innovators / classmates from top schools","Day trips to Oxford/London"]
+  
+const List = ({items})=>{
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+const ImagesGrid = ()=> {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-8">
+      {images.map((image, index) => (
+        <img className={`rounded-lg ${index > 1 ? 'hidden sm:block' : ''}`} src={image} alt="" key={index} />
+      ))}
+    </div>
+  );
+}
 
 const Cye = () => {
   return (
     <div>
-      <div className="bg-cover bg-top bg-fixed bg-gray-600 bg-blend-overlay"
-      style={{ backgroundImage: "url(../../../src/assets/images/cye/cye_top.jpg)" }}>
+      <div
+        className="bg-cover bg-top bg-fixed bg-gray-600 bg-blend-overlay"
+        style={{
+          backgroundImage:  `url(${cyetop})`,
+        }}
+      >
         <div className="max-w-[1280px] mx-auto p-8 text-center sm:text-left">
           <div className="lg:max-w-[50%]">
-            <p className="text-sky-500 font-semibold">Business and Entrepreneurship Course</p>
+            <p className="text-sky-500 font-semibold">
+              Business and Entrepreneurship Course
+            </p>
             <h2 className="text-white">CAMBRIDGE YOUNG ENTREPRENEURS (CYE)</h2>
             <p className="text-white">
-              Situated in the historic town of Cambridge, the course combines both
-              academic knowledge for university and key skills for future careers. It
-              is perfect for gaining an introduction into the study of
-              entrepreneurship at the university level and develop skills essential
-              for success such as leadership skills and communication skills. Here,
-              you will get to study with prominent academics from the world-renowned
-              University of Cambridge, live in its centuries-old college
-              accommodations just like their students, dine at its historic dining
-              halls, meet bright young minds from Cambridge who would be your
-              supervisors and connect with classmates. This truly is a
-              once-in-a-lifetime experience that you wouldn’t want to miss.
+              {hero}
             </p>
           </div>
         </div>
@@ -49,95 +88,47 @@ const Cye = () => {
       </div>
       {/* course outline */}
       <div className="flex justify-center mt-8">
-        <button className="btn">Course Outline</button>
+        <PopOut text="Course Outline" content_img={cyecourseoutline} />
       </div>
+      
 
       <div className="max-w-[1280px] mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4 p-8">
         <div>
           <h3>CYE Course Outline</h3>
           <h4 className="mt-4">Topic</h4>
-          <ul>
-            <li>Entrepreneurship fundamentals</li>
-            <li>Innovation and “Design Thinking”</li>
-            <li>Creating differentiated products and services</li>
-            <li>Understanding customers</li>
-            <li>Growth strategies</li>
-            <li>Business model innovation</li>
-            <li>Finance</li>
-            <li>Business plan preparation</li>
-          </ul>
+          <List items={topics} />
+
           <h4 className="mt-4">Teaching Approaches</h4>
-          <ul>
-            <li>Lectures</li>
-            <li>Oxbridge-style supervisions</li>
-            <li>In-class experiential activities</li>
-            <li>Project-based learning</li>
-            <li>
-              Small group workshops(with local business & start-up companies
-            </li>
-          </ul>
+          <List items={teachingApproaches} />
+          
         </div>
         <div>
           <h3>Programme Fees:</h3>
           <h3>Available upon request</h3>
           <h4 className="mt-4">What is Included:</h4>
-          <ul>
-            <li>Course Fees </li>
-            <li>
-              All teaching, small group seminars, and skills development
-              workshops
-            </li>
-            <li>Accommodation for 14 nights in college room </li>
-            <li>Venue and study materials</li>
-            <li>3 meals a day </li>
-            <li>Formal Dinner (Cambridge’s traditional college banquet)</li>
-            <li>All programmed visits and excursions </li>
-            <li>All transportation including airport transfers</li>
-            <li>A graduation ceremony</li>
-            <li>A pre-departure orientation</li>
-            <li>Travel insurance</li>
-            <li>
-              A supervisor accompanying the group throughout the programme from
-              airport departure to home arrival
-            </li>
-          </ul>
-          <h4 className="mt-4">What is <span className="text-red-500 ">Not</span> Included:</h4>
-          <ul>
-            <li>International fligh ticket</li>
-            <li>Visa application fees (if applicable)</li>
-          </ul>
+          <List items={includeItems} />
+
+          <h4 className="mt-4">
+            What is <span className="text-red-500 ">Not</span> Included:
+          </h4>
+          <List items={notIncludeItems} />
+
         </div>
       </div>
 
       {/* highlight */}
       <div className="bg-gray-100 ">
         <div className="max-w-[1280px] mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4 pb-8 mt-8 items-center">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-8">
-              <img className="rounded-lg" src={cye1} alt="" />
-              <img className="rounded-lg" src={cye2} alt="" />
-              <img className="hidden sm:block rounded-lg" src={cye3} alt="" />
-              <img className="hidden sm:block rounded-lg" src={cye4} alt="" />
-              <img className="hidden sm:block rounded-lg" src={cye5} alt="" />
-              <img className="hidden sm:block rounded-lg" src={cye6} alt="" />
-          </div>
+          <ImagesGrid />
+
           <div className="px-8">
             <h3>Highlights</h3>
             <h4 className="mt-4">Experience Business and Entrepreneurship</h4>
             <h4>Experience Leadership and Public Speaking</h4>
             <ul className="sm:leading-loose">
-              <li>Taught by Cambridge professors/lecturers</li>
-              <li>Certificate of attendance</li>
-              <li>University-level subject in a campus environment</li>
-              <li>Mini Hackathon experience (the winning team gets a prize)</li>
-              <li>Workshops with local businesses and start-up companies</li>
-              <li>Skills development workshops for future success</li>
-              <li>Real-life experience as a Cambridge student</li>
-              <li>
-                Networking opportunities with Cambridge faculties & students/
-                successful entrepreneurs & innovators / classmates from top
-                schools
-              </li>
-              <li>Day trips to Oxford/London</li>
+            {highlights.map((topic, index) => (
+                <li key={index}>{topic}</li>
+            ))}
             </ul>
           </div>
         </div>
